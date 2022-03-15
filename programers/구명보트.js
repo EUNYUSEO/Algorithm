a11523632d6dbd9eb431b3bacc8593687b7002b8
EUNYUSEO <eyseo328@gmail.com>
2022-03-15 14:07:00 +0900

    solve: 투 포인트 알고리즘

--- programers/구명보트.js
+++ programers/구명보트.js
@@ -4,6 +4,7 @@ const limit1 = 100;
 const limit2 = 100;
 
 function solution(people, limit) {
+  //! 첫번째 생각
   // limit은 people의 최대값보다 항상 크므로 people을 오름차순으로 정렬 풀기
   // 큐 이용하여 풀기
   // 큐에 people의 요소를 차례로 삽입
@@ -12,26 +13,52 @@ function solution(people, limit) {
   // 큐의 요소 합이 limit보다 크면 큐의 마지막 요소는 다시 people 첫번째 요소로 삽입
   // -> 더이상 보트에 못 태운다 판단, 카운트 1 증가 -> 큐 비우기
 
+  // people.sort((a, b) => b - a);
+  // let queue = [];
+  // let count = 0;
+
+  // while (people.length > 0) {
+  //   queue.push(people.shift());
+
+  //   let sum = queue.reduce((pre, cur) => pre + cur, 0);
+
+  //   if (people.length === 0 && sum < limit) {
+  //     count++;
+  //     break;
+  //   } else if (sum === limit) {
+  //     count++;
+  //     queue = [];
+  //   } else if (sum > limit) {
+  //     people.unshift(queue.pop());
+  //     count++;
+  //     queue = [];
+  //   }
+  // }
+
+  // return count;
+  // --------------------------------------------------------
+
+  // ! 두번째 생각
+  // people = [70, 50, 80, 50];
+  // limit = 100;
+  // people을 내림차순으로 정렬
+  // people 첫번째 요소 + people 마지막 요소의 값이
+  // limit보다 크면 people 첫번째 요소는 혼자 탈 수 밖에 없다고 판단 -> count 1 증가, 다음부턴 people 두번째 요소 + people 마지막 요소로 비교
+  // limit보다 작으면 둘 다 같이 탈 수 있다고 판단
+  // -> count 1 증가, 다음부턴 people 두번째 요소 + people 마지막 전 요소로 비교
+
   people.sort((a, b) => b - a);
-  let queue = [];
+
+  let start = 0;
+  let end = people.length - 1;
   let count = 0;
 
-  while (people.length > 0) {
-    queue.push(people.shift());
+  while (start <= end) {
+    const sum = people[start] + people[end];
 
-    let sum = queue.reduce((pre, cur) => pre + cur, 0);
-
-    if (people.length === 0 && sum < limit) {
-      count++;
-      break;
-    } else if (sum === limit) {
-      count++;
-      queue = [];
-    } else if (sum > limit) {
-      people.unshift(queue.pop());
-      count++;
-      queue = [];
-    }
+    if (sum <= limit) end--;
+    start++;
+    count++;
   }
 
   return count;
